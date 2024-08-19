c0721c6e50b85ba1ed22f607539a36a27c1d11f3
GlendyT <glendytuyuc@gmail.com>
2024-08-19 15:05:57 -0600

    instalando stackoverflow, zustand

---
+++ src/presentation/navigation/StackNavigator.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {LoadingScreen} from '../screens/loading/LoadingScreen';
+import {PermissionScreen} from '../screens/permissions/PermissionScreen';
+import {MapScreen} from '../screens/maps/MapScreen';
+
+export type RootStackParams = {
+  LoadingScreen: undefined;
+  PermissionScreen: undefined;
+  MapScreen: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      //initialRouteName="LoadingScreen"
+      initialRouteName="PermissionScreen"
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: 'white',
+        },
+      }}>
+      <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+      <Stack.Screen name="MapScreen" component={MapScreen} />
+      <Stack.Screen name="PermissionScreen" component={PermissionScreen} />
+    </Stack.Navigator>
+  );
+};
