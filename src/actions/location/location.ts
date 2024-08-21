0c13903e5fdf3b61d57f705786c1caa04bdd9858
GlendyT <glendytuyuc@gmail.com>
2024-08-21 08:03:47 -0600

    agregar botones para visulaizar la locacion del usuario, agregar funciones en zustand

---
+++ src/actions/location/location.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Geolocation from '@react-native-community/geolocation';
+import {Location} from '../../infrastructure/interfaces/location';
+
+export const getCurrentLocation = async (): Promise<Location> => {
+  return new Promise((resolve, reject) => {
+    Geolocation.getCurrentPosition(
+      info => {
+        resolve({
+          latitude: info.coords.latitude,
+          longitude: info.coords.longitude,
+        });
+      },
+      error => {
+        console.log(`Cannot get location: ${error}`);
+        reject(error);
+      },
+      {
+        enableHighAccuracy: true,
+      },
+    );
+  });
+};
+
+export const watchCurrentLocation = (
+  locationCallback: (location: Location) => void,
+): number => {
+  return Geolocation.watchPosition(
+    info =>
+      locationCallback({
+        latitude: info.coords.latitude,
+        longitude: info.coords.longitude,
+      }),
+    error => {
+      throw new Error('Cannot get watchPosition');
+    },
+    {
+      enableHighAccuracy: true,
+    },
+  );
+};
+
+export const clearWatchLocation = (watchId: number) => {
+  Geolocation.clearWatch(watchId);
+};
